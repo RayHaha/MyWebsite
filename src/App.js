1610743d8fbfa764c7e534f661309301b597bfc0
Yu-Jui Chang <y.chang@sany.org>
2023-11-30 09:15:12 -0500

    add navbar and theme mode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
-function App() {
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { useTheme, ThemeProvider, createTheme } from "@mui/material/styles";
+import NavBar from "./components/NavBar";
+
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+
+function MyApp() {
+  const theme = useTheme();
+  const colorMode = React.useContext(ColorModeContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>My Personal Website</p>
-      </header>
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+        bgcolor: "background.default",
+        color: "text.primary",
+        borderRadius: 1,
+      }}
+    >
+      <NavBar
+        mode={theme.palette.mode}
+        themeOnClick={colorMode.toggleColorMode}
+      />
+    </Box>
   );
 }
 
-export default App;
+export default function ToggleColorMode() {
+  const [mode, setMode] = React.useState("dark");
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode]
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <MyApp />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
